0cf1ce75e61ce59b4d8e146e64dbd01773f41c01
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-04-23 10:44:40 +0200

    first upload

---
+++ clock.js
@@ -0,0 +1,20 @@
+//display time
+function realtimeClock() {
+    var rtClock = new Date();
+
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+
+    document.getElementById('clock').innerHTML = hours + " : " + minutes;
+    var t = setTimeout(realtimeClock, 500);
+}
+
+//color picker
+function changecolor(color) {
+    document.body.style.backgroundColor = color.value;
+}
+
+
