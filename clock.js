2eb594df72e4b99142395f2f8acf653b0572e99d
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-07-02 13:02:50 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 //display time
-function realtimeClock() {
+function realtimeClock(){
     var rtClock = new Date();
 
     var hours = rtClock.getHours();
@@ -10,12 +10,25 @@ function realtimeClock() {
 
     document.getElementById('clock').innerHTML = hours + " : " + minutes;
     var t = setTimeout(realtimeClock, 500);
+
+    date = new Date();
+    year = date.getFullYear();
+    day = date.getDate();
+
+    const weekday = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"];
+    const monthday = ["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"];
+
+    let week = weekday[date.getDay()];
+    let month = monthday[date.getUTCMonth()];
+
+    document.getElementById("data").innerHTML = week + " " + day + " " + month;
 }
 
 //color picker
-function changecolor(color) {
+function changecolor(color){
     document.body.style.backgroundColor = color.value;
 }
 
-//credits
-console.log("Created by Hemerale Back");
+//console
+console.log('%cCreated by HemeraleBack', "font-size: 30px; color: #A39FC8;")
+console.log('%chttps://github.com/hemeraleback/Guidaldi', "font-size: 15px;")
